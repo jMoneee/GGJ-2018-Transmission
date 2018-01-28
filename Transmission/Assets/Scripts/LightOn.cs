e100e4c711af431582a385b8e52b9219987bafb0
jMoneee <jdlarse1@asu.edu>
2018-01-28 12:50:21 -0700

    actually fully implemented the light changing script

--- Transmission/Assets/Scripts/LightOn.cs
+++ Transmission/Assets/Scripts/LightOn.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class LightOn : MonoBehaviour {
+public class lightOn : MonoBehaviour {
 
-	public int index; //set this to 1-4 depending on line or 5 if direct phone line
-	public Sprite lightOn;
-	public Sprite lightOff;
+	public int index; //set this to 0-3 depending on line or 4 if direct phone line
+	public Sprite lightOnSprite;
+	public Sprite lightOffSprite;
 
 	int state;
 	Image lightImage;
@@ -21,16 +21,16 @@ public class LightOn : MonoBehaviour {
 	void Update () {
 		
 	}
-	public void getCall(){ //call this when call is set to come in
+	public void incomingCall(){ //call this when call is set to come in
 		if (state == 0) {
-			lightImage.sprite = lightOff;
+			lightImage.sprite = lightOffSprite;
 			state = 1;
 
 		}
 	}
 	public void endCall(){ //call this when a call is scripted to end
 		if (state == 1) {
-			lightImage.sprite = lightOn;
+			lightImage.sprite = lightOnSprite;
 			state = 0;
 		}
 	
