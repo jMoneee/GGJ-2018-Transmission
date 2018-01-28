c46e90dea83d73953e974f645c7800a6d761ad31
jMoneee <jdlarse1@asu.edu>
2018-01-28 15:17:00 -0700

    Implemented final main menu and credits

---
+++ Transmission/Assets/CreditsExit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CreditsExit : MonoBehaviour {
+
+	public Button yourButton;
+	public GameObject myPanel;
+
+	// Use this for initialization
+	void Start () {
+		Button btn = yourButton.GetComponent<Button> ();
+		btn.onClick.AddListener (TaskOnClick);
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+	void TaskOnClick(){
+		myPanel.SetActive(false);
+	}
+
+
+}
